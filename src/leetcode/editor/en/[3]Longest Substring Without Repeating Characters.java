e45cde20f6d412a6a1f020f6e3d5adaccd9d7a39
周洲游 <1445352275@qq.com>
2020-01-12 23:26:19 +0800

    [3]Longest Substring Without Repeating Characters: Sliding Window using HashSet

---
+++ src/leetcode/editor/en/[3]Longest Substring Without Repeating Characters.java
@@ -0,0 +1,59 @@
+package leetcode.editor.en;
+//Given a string, find the length of the longest substring without repeating cha
+//racters. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: "abcabcbb"
+//Output: 3 
+//Explanation: The answer is "abc", with the length of 3. 
+// 
+//
+// 
+// Example 2: 
+//
+// 
+//Input: "bbbbb"
+//Output: 1
+//Explanation: The answer is "b", with the length of 1.
+// 
+//
+// 
+// Example 3: 
+//
+// 
+//Input: "pwwkew"
+//Output: 3
+//Explanation: The answer is "wke", with the length of 3. 
+//             Note that the answer must be a substring, "pwke" is a subsequence
+// and not a substring.
+// 
+// 
+// 
+// 
+// Related Topics Hash Table Two Pointers String Sliding Window
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0, i = 0, j = 0;
+        int l = s.length();
+        Set<Character> set = new HashSet<>();
+        while (i < l && j < l) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                max = Math.max(max, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return max;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
