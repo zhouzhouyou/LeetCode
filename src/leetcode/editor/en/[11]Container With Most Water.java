c4624e90711dae8370df9410853b556e3b1ac35a
周洲游 <1445352275@qq.com>
2020-01-27 13:08:43 +0800

    [11]Container With Most Water 滑动窗口？

---
+++ src/leetcode/editor/en/[11]Container With Most Water.java
@@ -0,0 +1,37 @@
+//Given n non-negative integers a1, a2, ..., an , where each represents a point 
+//at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of
+// line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis for
+//ms a container, such that the container contains the most water. 
+//
+// Note: You may not slant the container and n is at least 2. 
+//
+// 
+//
+// 
+//
+// The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In thi
+//s case, the max area of water (blue section) the container can contain is 49. 
+//
+// 
+//
+// Example: 
+//
+// 
+//Input: [1,8,6,2,5,4,8,3,7]
+//Output: 49 Related Topics Array Two Pointers
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxArea(int[] height) {
+        int start = 0, end = height.length - 1;
+        int max = 0;
+        while (start < end) {
+            max = Math.max(max, Math.min(height[start], height[end]) * (end - start));
+            if (height[start] < height[end]) start++;
+            else end--;
+        }
+        return max;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
