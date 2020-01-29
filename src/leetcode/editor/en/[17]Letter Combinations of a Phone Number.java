833fbd97a757c7b7c40325bc04b2621482ad6eed
周洲游 <1445352275@qq.com>
2020-01-29 21:19:44 +0800

    [17]Letter Combinations of a Phone Number 网上的方法。回溯遍历，StringBuilder

--- src/leetcode/editor/en/[17]Letter Combinations of a Phone Number.java
+++ src/leetcode/editor/en/[17]Letter Combinations of a Phone Number.java
@@ -20,36 +20,41 @@
 // Related Topics String Backtracking
 
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<String> letterCombinations(String digits) {
-        List<String> list = new ArrayList<>();
-        if (digits == null || digits.length() == 0) return list;
-        char[][] map = new char[8][];
-        map[0]="abc".toCharArray();
-        map[1]="def".toCharArray();
-        map[2]="ghi".toCharArray();
-        map[3]="jkl".toCharArray();
-        map[4]="mno".toCharArray();
-        map[5]="pqrs".toCharArray();
-        map[6]="tuv".toCharArray();
-        map[7]="wxyz".toCharArray();
+        List<String> result = new LinkedList<>();
+        if(digits==null || digits.length()==0) return result;
+        Map<Integer,String> mapping = new HashMap<>();
+        mapping.put(2,"abc");
+        mapping.put(3,"def");
+        mapping.put(4,"ghi");
+        mapping.put(5,"jkl");
+        mapping.put(6,"mno");
+        mapping.put(7,"pqrs");
+        mapping.put(8,"tuv");
+        mapping.put(9,"wxyz");
 
-        char[] digit = digits.toCharArray();
-        list.add("");
-        for (char c : digit) {
-            List<String> temp = new ArrayList<>();
-            for (String s : list) {
-                for (char cc : map[c - '2']) {
-                    temp.add(s + cc);
-                }
-            }
-            list = temp;
+        populateLetterCombinations(digits, mapping, new StringBuilder(),0, result);
+        return result;
+    }
+
+
+    private void populateLetterCombinations(String digits, Map<Integer,String> mapping, StringBuilder temp, int index, List<String> result){
+        if(temp.length() == digits.length()){
+            //如果长度一样了，说明到了末尾，那么就把temp变为String存入
+            result.add(temp.toString());
+            return;
         }
-        return list;
+        String letters = mapping.get(digits.charAt(index)-'0');
+        for(int i=0;i<letters.length();i++){
+            temp.append(letters.charAt(i));
+            populateLetterCombinations(digits, mapping, temp, index+1, result);
+            //这一步已经到了最后，于是回退一步
+            temp.deleteCharAt(temp.length()-1);
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
