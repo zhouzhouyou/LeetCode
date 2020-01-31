9025c7760d8c8aa3869782b42889a1895ab6746f
周洲游 <1445352275@qq.com>
2020-01-31 14:10:31 +0800

    [19]Remove Nth Node From End of List 这个题目成功把我气死了。要求one pass，结果给的solution是两个指针

---
+++ src/leetcode/editor/en/[19]Remove Nth Node From End of List.java
@@ -0,0 +1,65 @@
+//Given a linked list, remove the n-th node from the end of list and return its 
+//head. 
+//
+// Example: 
+//
+// 
+//Given linked list: 1->2->3->4->5, and n = 2.
+//
+//After removing the second node from the end, the linked list becomes 1->2->3->
+//5.
+// 
+//
+// Note: 
+//
+// Given n will always be valid. 
+//
+// Follow up: 
+//
+// Could you do this in one pass? 
+// Related Topics Linked List Two Pointers
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.Stack;
+import java.util.Vector;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode start = new ListNode(0);
+        ListNode slow = start, fast = start;
+        slow.next = head;
+
+        //Move fast in front so that the gap between slow and fast becomes n
+        for(int i=1; i<=n+1; i++)   {
+            fast = fast.next;
+        }
+        //Move fast to the end, maintaining the gap
+        while(fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        //Skip the desired node
+        slow.next = slow.next.next;
+        return start.next;
+    }
+
+//    public class ListNode {
+//        int val;
+//        ListNode next;
+//
+//        ListNode(int x) {
+//            val = x;
+//        }
+//    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
