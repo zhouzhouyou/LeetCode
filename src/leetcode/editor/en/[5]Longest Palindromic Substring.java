3209c76f64d30bd1ddfe18f96984a4137cb9514e
周洲游 <1445352275@qq.com>
2020-02-03 15:29:34 +0800

    [5]Longest Palindromic Substring 真正的马拉车算法 (Manacher)，上次的是错的

--- src/leetcode/editor/en/[5]Longest Palindromic Substring.java
+++ src/leetcode/editor/en/[5]Longest Palindromic Substring.java
@@ -18,37 +18,42 @@
 // Related Topics String Dynamic Programming
 
 
+import java.util.Arrays;
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String longestPalindrome(String s) {
-        StringBuilder builder = new StringBuilder("~|");
-        for (char x : s.toCharArray()) {
-            builder.append(x).append('|');
+        int n = s.length();
+        int idx = 0, maxLen = 0;
+        StringBuffer sb = new StringBuffer();
+        sb.append('^');
+        for (int i = 0; i < n; ++i) {
+            sb.append('#');
+            sb.append(s.charAt(i));
         }
-        String str = builder.toString();
-        int[] array = new int[str.length()];
-        int max = 0;
-        for (int i = 0; i < str.length(); i++) {
-            array[i] = getLength(str, i);
+        sb.append("#$");
+        n = 2 * n + 3;
+        int right = 0, id_center = 0;
+        //记录了最右侧到达了哪里，和原点位置
+        int[] p = new int[n];
+        Arrays.fill(p,0);
+        for (int i = 1; i < n - 1; ++i) {
+            p[i] = (right > i) ? Math.min(p[2 * id_center - i], right - i) : 0;
+            //如果当前访问i超过了记录范围，那么就从0开始。
+            //否则，选取i关于center的对称点,有i+x=2*center，或者right-i(对称点的最左端比center最左端小的情况)
+            while (sb.charAt(i + 1 + p[i]) == sb.charAt(i - 1 - p[i])) ++p[i];
+            if (i + p[i] > right) {
+                //如果刷新了右端的话
+                id_center = i; right = i + p[i];
+            }
+            if (p[i] > maxLen) {
+                //如果刷新了最大值
+                idx = i; maxLen = p[i];
+            }
         }
-        for (int i = 0; i < str.length(); i++) {
-            if (array[i] > array[max]) max = i;
-        }
-        int left = (max - array[max]) / 2;
-        int right = left + array[max] - 1;
-        return s.substring(left, right);
-    }
-
-    public int getLength(String s, int l) {
-        int length = s.length();
-        int ans = 1;
-        if (l == 0 || l == length - 1) return 1;
-        int i = l - 1, j = l + 1;
-        while (i >= 0 && j < length) {
-            if (s.charAt(i--) != s.charAt(j++)) break;
-            ans++;
-        }
-        return ans;
+        //左端
+        idx = (idx - maxLen - 1) / 2;
+        return s.substring(idx, idx + maxLen);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
