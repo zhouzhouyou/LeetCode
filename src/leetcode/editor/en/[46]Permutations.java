5f0361fba5c8e423bfe497a3db9eac82a92ef6cc
周洲游 <1445352275@qq.com>
2020-02-05 12:45:48 +0800

    [46]Permutations 网上的方法，简短很多

--- src/leetcode/editor/en/[46]Permutations.java
+++ src/leetcode/editor/en/[46]Permutations.java
@@ -17,39 +17,29 @@
 // Related Topics Backtracking
 
 
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        if (nums.length == 0) {
-            return new LinkedList<>();
-        }
-        return backtrack(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        // Arrays.sort(nums); // not necessary
+        backtrack(list, new ArrayList<>(), nums);
+        return list;
     }
 
-    public List<List<Integer>> backtrack(int[] nums) {
-        List<List<Integer>> lists = new LinkedList<>();
-        if (nums.length == 1) {
-            lists.add(Collections.singletonList(nums[0]));
-            return lists;
-        }
-        for (int i = 0; i < nums.length; i++) {
-            int current = nums[i];
-            int[] integers = new int[nums.length - 1];
-            System.arraycopy(nums, 0, integers, 0, i);
-            for (int j = i + 1; j < nums.length; j++) integers[j - 1] = nums[j];
-            List<List<Integer>> temp = backtrack(integers);
-            for (List<Integer> list : temp) {
-                List<Integer> tempList = new LinkedList<>();
-                tempList.add(current);
-                tempList.addAll(list);
-                lists.add(tempList);
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums) {
+        if (tempList.size() == nums.length) {
+            list.add(new ArrayList<>(tempList));
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (tempList.contains(nums[i])) continue; // element already exists, skip
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums);
+                tempList.remove(tempList.size() - 1);
             }
         }
-        return lists;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
