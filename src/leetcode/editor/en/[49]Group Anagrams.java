0eec123a297ecc83998a9e1dbb78fcb70b618572
周洲游 <1445352275@qq.com>
2020-02-06 11:53:41 +0800

    [49]Group Anagrams 对每个字符串进行排序

---
+++ src/leetcode/editor/en/[49]Group Anagrams.java
@@ -0,0 +1,40 @@
+//Given an array of strings, group anagrams together. 
+//
+// Example: 
+//
+// 
+//Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+//Output:
+//[
+//  ["ate","eat","tea"],
+//  ["nat","tan"],
+//  ["bat"]
+//] 
+//
+// Note: 
+//
+// 
+// All inputs will be in lowercase. 
+// The order of your output does not matter. 
+// 
+// Related Topics Hash Table String
+
+
+import java.util.*;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String key = String.valueOf(c);
+            if (!map.containsKey(key)) map.put(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
