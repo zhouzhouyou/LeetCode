87dfc9d0bc8c782359838c075d3e2fb6810bac40
周洲游 <1445352275@qq.com>
2020-02-07 22:12:29 +0800

    [61]Rotate List 简单链表操作

---
+++ src/leetcode/editor/en/[61]Rotate List.java
@@ -0,0 +1,69 @@
+//Given a linked list, rotate the list to the right by k places, where k is non-
+//negative. 
+//
+// Example 1: 
+//
+// 
+//Input: 1->2->3->4->5->NULL, k = 2
+//Output: 4->5->1->2->3->NULL
+//Explanation:
+//rotate 1 steps to the right: 5->1->2->3->4->NULL
+//rotate 2 steps to the right: 4->5->1->2->3->NULL
+// 
+//
+// Example 2: 
+//
+// 
+//Input: 0->1->2->NULL, k = 4
+//Output: 2->0->1->NULL
+//Explanation:
+//rotate 1 steps to the right: 2->0->1->NULL
+//rotate 2 steps to the right: 1->2->0->NULL
+//rotate 3 steps to the right: 0->1->2->NULL
+//rotate 4 steps to the right: 2->0->1->NULL 
+// Related Topics Linked List Two Pointers
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null) return null;
+        ListNode fake = new ListNode(0);
+        fake.next = head;
+        ListNode rear = head;
+        int count = 1;
+        while (rear.next != null) {
+            rear = rear.next;
+            count++;
+        }
+        k %= count;
+        if (k == 0) return head;
+        ListNode p = head;
+        for (int i = 1; i < count - k; i++) {
+            p = p.next;
+        }
+        ListNode newHead = p.next;
+        rear.next = head;
+        p.next = null;
+        return newHead;
+    }
+
+//    public class ListNode {
+//        int val;
+//        ListNode next;
+//
+//        ListNode(int x) {
+//            val = x;
+//        }
+//    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
