c9004be8bc43e2cd822ccce2e8e8ccb3749db127
周洲游 <1445352275@qq.com>
2020-02-07 23:38:55 +0800

    [91]Decode Ways 其实这个题目很简单，但是边界很麻烦，所以评分低~，所以我懒得做了，直接拿了别人的

---
+++ src/leetcode/editor/en/[91]Decode Ways.java
@@ -0,0 +1,55 @@
+//A message containing letters from A-Z is being encoded to numbers using the fo
+//llowing mapping: 
+//
+// 
+//'A' -> 1
+//'B' -> 2
+//...
+//'Z' -> 26
+// 
+//
+// Given a non-empty string containing only digits, determine the total number o
+//f ways to decode it. 
+//
+// Example 1: 
+//
+// 
+//Input: "12"
+//Output: 2
+//Explanation: It could be decoded as "AB" (1 2) or "L" (12).
+// 
+//
+// Example 2: 
+//
+// 
+//Input: "226"
+//Output: 3
+//Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6)
+//. 
+// Related Topics String Dynamic Programming
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int numDecodings(String s) {
+        if(s.length() == 0) {
+            return 0;
+        }
+        int n = s.length();
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) != '0' ? 1 : 0;
+        for(int i = 2; i <= n; i++) {
+            int first = Integer.parseInt(s.substring(i-1, i));
+            int second = Integer.parseInt(s.substring(i-2, i));
+            if(first >= 1 && first <= 9) {
+                dp[i] += dp[i-1];
+            }
+            if(second >= 10 && second <= 26) {
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
