c0b2c0751f3795900b8ba85984e124ccde506c03
周洲游 <1445352275@qq.com>
2020-02-09 15:37:02 +0800

    [75]Sort Colors one-pass

--- src/leetcode/editor/en/[75]Sort Colors.java
+++ src/leetcode/editor/en/[75]Sort Colors.java
@@ -29,15 +29,20 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public void sortColors(int[] nums) {
-        int count0 = 0, count1 = 0;
-        for (int num : nums) {
-            if (num == 0) count0++;
-            if (num == 1) count1++;
-        }
-        for(int i = 0; i < nums.length; i++) {
-            if (i < count0) {nums[i] = 0;}
-            else if (i < count0 + count1) {nums[i] = 1;}
-            else {nums[i] = 2;}
+        int p1 = 0, p2 = nums.length - 1, index = 0;
+        while (index <= p2) {
+            if (nums[index] == 0) {
+                nums[index] = nums[p1];
+                nums[p1] = 0;
+                p1++;
+            }
+            if (nums[index] == 2) {
+                nums[index] = nums[p2];
+                nums[p2] = 2;
+                p2--;
+                index--;
+            }
+            index++;
         }
     }
 }
