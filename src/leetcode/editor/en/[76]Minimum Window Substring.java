c08df57fa46f51fe5846fbbb07ac2f07d289e84b
周洲游 <1445352275@qq.com>
2020-02-10 17:22:59 +0800

    [76]Minimum Window Substring    题解的方案。直接滤过了不存在的字符

--- src/leetcode/editor/en/[76]Minimum Window Substring.java
+++ src/leetcode/editor/en/[76]Minimum Window Substring.java
@@ -19,44 +19,86 @@
 // Related Topics Hash Table Two Pointers String Sliding Window
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String minWindow(String s, String t) {
         if (s.length() == 0 || t.length() == 0) return "";
-        Map<Character, Integer> dict = new HashMap<>();
-        for (char x : t.toCharArray()) dict.put(x, dict.getOrDefault(x, 0) + 1);
-        int[] flags = {Integer.MAX_VALUE, 0, 0};
-        int left = 0, right = 0;
 
-        Map<Character, Integer> window = new HashMap<>(dict);
+        Map<Character, Integer> dictT = new HashMap<>();
 
-        while (right <= s.length() && left <= right) {
-            boolean flag = true;
-            for (char target : dict.keySet()) {
-                if (window.get(target) > 0) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) {
-                if (right - left < flags[0]) {
-                    flags[0] = right - left + 1;
-                    flags[1] = left; flags[2] = right;
-                }
-                char remove = s.charAt(left++);
-                window.put(remove, window.get(remove) + 1);
-            } else {
-                if (right < s.length()) {
-                    char x = s.charAt(right++);
-                    window.put(x, window.getOrDefault(x, 0) - 1);
-                }
-                else break;
-            }
+        for (int i = 0; i < t.length(); i++) dictT.put(t.charAt(i), dictT.getOrDefault(t.charAt(i), 0) + 1);
+
+        int required = dictT.size();
+
+        // Filter all the characters from s into a new list along with their index.
+        // The filtering criteria is that the character should be present in t.
+        List<Pair<Integer, Character>> filteredS = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (dictT.containsKey(c)) filteredS.add(new Pair<>(i, c));
         }
-        return flags[0] == -1 ? "" : s.substring(flags[1], flags[2]);
+
+        int l = 0, r = 0, formed = 0;
+        Map<Character, Integer> windowCounts = new HashMap<>();
+        int[] ans = {-1, 0, 0};
+
+        // Look for the characters only in the filtered list instead of entire s.
+        // This helps to reduce our search.
+        // Hence, we follow the sliding window approach on as small list.
+        while (r < filteredS.size()) {
+            char c = filteredS.get(r).getValue();
+            int count = windowCounts.getOrDefault(c, 0);
+            windowCounts.put(c, count + 1);
+
+            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
+                formed++;
+            }
+
+            // Try and contract the window till the point where it ceases to be 'desirable'.
+            while (l <= r && formed == required) {
+                c = filteredS.get(l).getValue();
+
+                // Save the smallest window until now.
+                int end = filteredS.get(r).getKey();
+                int start = filteredS.get(l).getKey();
+                if (ans[0] == -1 || end - start + 1 < ans[0]) {
+                    ans[0] = end - start + 1;
+                    ans[1] = start;
+                    ans[2] = end;
+                }
+
+                windowCounts.put(c, windowCounts.get(c) - 1);
+                if (dictT.containsKey(c) && windowCounts.get(c) < dictT.get(c)) {
+                    formed--;
+                }
+                l++;
+            }
+            r++;
+        }
+        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+    }
+
+    private class Pair<T, S> {
+        T t;
+        S s;
+
+        private Pair (T t, S s) {
+            this.t = t;
+            this.s = s;
+        }
+
+        public T getKey() {
+            return t;
+        }
+
+        public S getValue() {
+            return s;
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
