8d89f10822ecbb03b1df565cb690a0dacb635cbf
周洲游 <1445352275@qq.com>
2020-02-11 23:31:46 +0800

    [108]Convert Sorted Array to Binary Search Tree 分而治之

---
+++ src/leetcode/editor/en/[108]Convert Sorted Array to Binary Search Tree.java
@@ -0,0 +1,59 @@
+//Given an array where elements are sorted in ascending order, convert it to a h
+//eight balanced BST. 
+//
+// For this problem, a height-balanced binary tree is defined as a binary tree i
+//n which the depth of the two subtrees of every node never differ by more than 1.
+// 
+//
+// Example: 
+//
+// 
+//Given the sorted array: [-10,-3,0,5,9],
+//
+//One possible answer is: [0,-3,9,-10,null,5], which represents the following he
+//ight balanced BST:
+//
+//      0
+//     / \
+//   -3   9
+//   /   /
+// -10  5
+// 
+// Related Topics Tree Depth-first Search
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode helper(int[] nums, int left, int right) {
+        if (left > right) return null;
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+        return root;
+    }
+
+//    public class TreeNode {
+//        int val;
+//        TreeNode left;
+//        TreeNode right;
+//
+//        TreeNode(int x) {
+//            val = x;
+//        }
+//    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
