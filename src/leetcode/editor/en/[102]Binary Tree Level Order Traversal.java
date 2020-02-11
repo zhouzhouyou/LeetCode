a703661ba599d14f964c63e33f4dbedccd220fc5
周洲游 <1445352275@qq.com>
2020-02-11 20:29:06 +0800

    [102]Binary Tree Level Order Traversal 递归

---
+++ src/leetcode/editor/en/[102]Binary Tree Level Order Traversal.java
@@ -0,0 +1,67 @@
+//Given a binary tree, return the level order traversal of its nodes' values. (i
+//e, from left to right, level by level). 
+//
+// 
+//For example: 
+//Given binary tree [3,9,20,null,null,15,7], 
+// 
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+// 
+// 
+//return its level order traversal as: 
+// 
+//[
+//  [3],
+//  [9,20],
+//  [15,7]
+//]
+// 
+// Related Topics Tree Breadth-first Search
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> lists = new LinkedList<>();
+        traversal(lists, root, 1);
+        return lists;
+    }
+
+    private void traversal(List<List<Integer>> lists, TreeNode root, int k) {
+        if (root == null) return;
+        if (lists.size() < k) lists.add(new LinkedList<>());
+        lists.get(k-1).add(root.val);
+
+        traversal(lists, root.left, k+1);
+        traversal(lists, root.right, k+1);
+    }
+
+//    public class TreeNode {
+//        int val;
+//        TreeNode left;
+//        TreeNode right;
+//
+//        TreeNode(int x) {
+//            val = x;
+//        }
+//    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
