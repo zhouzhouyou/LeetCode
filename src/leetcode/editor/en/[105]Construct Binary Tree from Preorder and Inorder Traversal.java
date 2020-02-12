71626ebf97fba1194c7384642a9547aa8cdf16e7
周洲游 <1445352275@qq.com>
2020-02-12 13:23:38 +0800

    [105]Construct Binary Tree from Preorder and Inorder Traversal 分而治之

---
+++ src/leetcode/editor/en/[105]Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -0,0 +1,69 @@
+//Given preorder and inorder traversal of a tree, construct the binary tree. 
+//
+// Note: 
+//You may assume that duplicates do not exist in the tree. 
+//
+// For example, given 
+//
+// 
+//preorder = [3,9,20,15,7]
+//inorder = [9,3,15,20,7] 
+//
+// Return the following binary tree: 
+//
+// 
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+// Related Topics Array Tree Depth-first Search
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        Map<Integer, Integer> inMap = new HashMap<>();
+
+        for(int i = 0; i < inorder.length; i++) inMap.put(inorder[i], i);
+
+        return buildTree(preorder, 0, preorder.length - 1, 0, inorder.length - 1, inMap);
+    }
+
+    public TreeNode buildTree(int[] preorder, int preStart, int preEnd, int inStart, int inEnd, Map<Integer, Integer> inMap) {
+        if(preStart > preEnd || inStart > inEnd) return null;
+
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int inRoot = inMap.get(root.val);
+        int numsLeft = inRoot - inStart;
+
+        root.left = buildTree(preorder, preStart + 1, preStart + numsLeft, inStart, inRoot - 1, inMap);
+        root.right = buildTree(preorder, preStart + numsLeft + 1, preEnd, inRoot + 1, inEnd, inMap);
+
+        return root;
+    }
+
+//    public class TreeNode {
+//        int val;
+//        TreeNode left;
+//        TreeNode right;
+//
+//        TreeNode(int x) {
+//            val = x;
+//        }
+//    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
