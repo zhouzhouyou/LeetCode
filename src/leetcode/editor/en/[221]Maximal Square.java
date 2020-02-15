07c17027459edf9441a897d871111fab5d780ce7
周洲游 <1445352275@qq.com>
2020-02-15 18:52:12 +0800

    [221]Maximal Square DP

---
+++ src/leetcode/editor/en/[221]Maximal Square.java
@@ -0,0 +1,35 @@
+//Given a 2D binary matrix filled with 0's and 1's, find the largest square cont
+//aining only 1's and return its area. 
+//
+// Example: 
+//
+// 
+//Input: 
+//
+//1 0 1 0 0
+//1 0 1 1 1
+//1 1 1 1 1
+//1 0 0 1 0
+//
+//Output: 4
+// Related Topics Dynamic Programming
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
+        int[][] dp = new int[rows+1][cols+1];
+        int max = 0;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (matrix[i-1][j-1] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        return max * max;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
