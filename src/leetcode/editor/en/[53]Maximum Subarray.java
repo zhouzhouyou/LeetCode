5810e4c01df7ef2c499ea78a8f6e8c74216f0556
周洲游 <1445352275@qq.com>
2020-02-18 11:58:25 +0800

    [53]Maximum Subarray 换了个算法，空间变为O(N)

--- src/leetcode/editor/en/[53]Maximum Subarray.java
+++ src/leetcode/editor/en/[53]Maximum Subarray.java
@@ -20,12 +20,11 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int l = nums.length;
-        int[] dp = new int[l];
-        dp[0] = nums[0];
-        int max = dp[0];
+        int sum = nums[0];
+        int max = sum;
         for (int i = 1; i < l; i++) {
-            dp[i] = dp[i-1] > 0 ? dp[i-1] + nums[i] : nums[i];
-            max = Math.max(max, dp[i]);
+            sum = (sum > 0 ? sum + nums[i] : nums[i]);
+            max = Math.max(max, sum);
         }
         return max;
     }
