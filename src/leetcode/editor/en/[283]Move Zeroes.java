a0a7d60fc3416f6acc6ed90f1d3ffb4d386e84c0
周洲游 <1445352275@qq.com>
2020-02-19 23:49:26 +0800

    [283]Move Zeroes 记录遇到过多少0

---
+++ src/leetcode/editor/en/[283]Move Zeroes.java
@@ -0,0 +1,31 @@
+//Given an array nums, write a function to move all 0's to the end of it while m
+//aintaining the relative order of the non-zero elements. 
+//
+// Example: 
+//
+// 
+//Input: [0,1,0,3,12]
+//Output: [1,3,12,0,0] 
+//
+// Note: 
+//
+// 
+// You must do this in-place without making a copy of the array. 
+// Minimize the total number of operations. 
+// Related Topics Array Two Pointers
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) count++;
+            else nums[i-count] = nums[i];
+        }
+        for (int i = nums.length - count; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
