5a1de7355c18f2a2f53aa9f321c460bd1fc1ab76
周洲游 <1445352275@qq.com>
2020-02-19 22:00:40 +0800

    [226]Invert Binary Tree 我反正也进不了Google

---
+++ src/leetcode/editor/en/[226]Invert Binary Tree.java
@@ -0,0 +1,62 @@
+//Invert a binary tree. 
+//
+// Example: 
+//
+// Input: 
+//
+// 
+//     4
+//   /   \
+//  2     7
+// / \   / \
+//1   3 6   9 
+//
+// Output: 
+//
+// 
+//     4
+//   /   \
+//  7     2
+// / \   / \
+//9   6 3   1 
+//
+// Trivia: 
+//This problem was inspired by this original tweet by Max Howell: 
+//
+// Google: 90% of our engineers use the software you wrote (Homebrew), but you c
+//an’t invert a binary tree on a whiteboard so f*** off. 
+// Related Topics Tree
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
+//    public class TreeNode {
+//        int val;
+//        TreeNode left;
+//        TreeNode right;
+//
+//        TreeNode(int x) {
+//            val = x;
+//        }
+//    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
