0978082090cbd3ef402cc91ac02aa1d81c2f8c98
周洲游 <1445352275@qq.com>
2020-02-20 16:15:57 +0800

    [138]Copy List with Random Pointer 正常人想不到~

---
+++ src/leetcode/editor/en/[138]Copy List with Random Pointer.java
@@ -0,0 +1,136 @@
+//A linked list is given such that each node contains an additional random point
+//er which could point to any node in the list or null. 
+//
+// Return a deep copy of the list. 
+//
+// The Linked List is represented in the input/output as a list of n nodes. Each
+// node is represented as a pair of [val, random_index] where: 
+//
+// 
+// val: an integer representing Node.val 
+// random_index: the index of the node (range from 0 to n-1) where random pointe
+//r points to, or null if it does not point to any node. 
+// 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+//Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: head = [[1,1],[2,1]]
+//Output: [[1,1],[2,1]]
+// 
+//
+// Example 3: 
+//
+// 
+//
+// 
+//Input: head = [[3,null],[3,0],[3,null]]
+//Output: [[3,null],[3,0],[3,null]]
+// 
+//
+// Example 4: 
+//
+// 
+//Input: head = []
+//Output: []
+//Explanation: Given linked list is empty (null pointer), so return null.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// -10000 <= Node.val <= 10000 
+// Node.random is null or pointing to a node in the linked list. 
+// Number of Nodes will not exceed 1000. 
+// 
+// Related Topics Hash Table Linked List
+
+
+import java.util.ArrayList;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+class Solution {
+    public Node copyRandomList(Node head) {
+        Node iter = head, next;
+
+        /*
+        第一步，将备份的next指向原链表的next，原链表的next指向备份
+         */
+        while (iter != null) {
+            next = iter.next;
+
+            Node copy = new Node(iter.val);
+            iter.next = copy;
+            copy.next = next;
+
+            iter = next;
+        }
+
+        /*
+        第二步，将备份的随机指向对应的备份
+         */
+        iter = head;
+        while (iter != null) {
+            if (iter.random != null) {
+                iter.next.random = iter.random.next;
+            }
+            iter = iter.next.next;
+        }
+
+        iter = head;
+        Node fake = new Node(0);
+        Node copy, copyIter = fake;
+
+        /*
+        第三部，将备份的next指向对应的next
+         */
+        while (iter != null) {
+            next = iter.next.next;
+
+            copy = iter.next;
+            copyIter.next = copy;
+            copyIter = copy;
+
+            iter.next = next;
+            iter = next;
+        }
+
+        return fake.next;
+    }
+
+//    class Node {
+//        int val;
+//        Node next;
+//        Node random;
+//
+//        public Node(int val) {
+//            this.val = val;
+//            this.next = null;
+//            this.random = null;
+//        }
+//    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
