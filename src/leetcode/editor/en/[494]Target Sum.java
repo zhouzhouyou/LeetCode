b1ee11279d1872791e82e37dd9029dbb0876abba
周洲游 <1445352275@qq.com>
2020-02-20 20:39:34 +0800

    [494]Target Sum DP Advanced

--- src/leetcode/editor/en/[494]Target Sum.java
+++ src/leetcode/editor/en/[494]Target Sum.java
@@ -37,19 +37,21 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int S) {
         int sum = 0;
-        for(int n: nums) sum += n;
-        if (S < -sum || S > sum) { return 0;}
-
-        int[][] dp = new int[nums.length + 1][ 2 * sum + 1];
-        dp[0][0 + sum] = 1; // 0 + sum means 0, 0 means -sum,  check below graph
-        for(int i = 1; i <= nums.length; i++){
-            for(int j = 0; j < 2 * sum + 1; j++){
-
-                if(j + nums[i - 1] < 2  * sum + 1) dp[i][j] += dp[i - 1][j + nums[i - 1]];
-                if(j - nums[i - 1] >= 0) dp[i][j] += dp[i - 1][j - nums[i - 1]];
+        for(int i: nums) sum+=i;
+        if(S>sum || S<-sum) return 0;
+        int[] dp = new int[2*sum+1];
+        dp[0+sum] = 1;
+        for (int num : nums) {
+            int[] next = new int[2 * sum + 1];
+            for (int k = 0; k < 2 * sum + 1; k++) {
+                if (dp[k] != 0) {
+                    next[k + num] += dp[k];
+                    next[k - num] += dp[k];
+                }
             }
+            dp = next;
         }
-        return dp[nums.length][sum + S];
+        return dp[sum+S];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
