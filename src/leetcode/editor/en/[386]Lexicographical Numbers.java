f05e29f9610ebdfc3f50b5a48670523b37f11284
周洲游 <1445352275@qq.com>
2020-02-27 19:02:51 +0800

    [386]Lexicographical Numbers dfs

---
+++ src/leetcode/editor/en/[386]Lexicographical Numbers.java
@@ -0,0 +1,32 @@
+//Given an integer n, return 1 - n in lexicographical order. 
+//
+// For example, given 13, return: [1,10,11,12,13,2,3,4,5,6,7,8,9]. 
+//
+// Please optimize your algorithm to use less time and space. The input size may
+// be as large as 5,000,000. 
+//
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i < 10; i++) {
+            dfs(i, n, list);
+        }
+        return list;
+    }
+
+    private void dfs(int cur, int n, List<Integer> list) {
+        if (cur > n) return;
+        list.add(cur);
+        cur *= 10;
+        for (int i = 0; i < 10; i++) {
+            if (cur + i <= n) dfs(cur + i, n, list);
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
