0a240f46363cca0c3af170cf6d611226c11a0789
周洲游 <1445352275@qq.com>
2020-04-05 14:13:06 +0800

    [147]Insertion Sort List 插入排序链表

---
+++ src/leetcode/editor/en/[147]Insertion Sort List.java
@@ -0,0 +1,82 @@
+//Sort a linked list using insertion sort. 
+//
+// 
+// 
+//
+// 
+//A graphical example of insertion sort. The partial sorted list (black) initial
+//ly contains only the first element in the list. 
+//With each iteration one element (red) is removed from the input data and inser
+//ted in-place into the sorted list 
+// 
+//
+// 
+// 
+//
+// Algorithm of Insertion Sort: 
+//
+// 
+// Insertion sort iterates, consuming one input element each repetition, and gro
+//wing a sorted output list. 
+// At each iteration, insertion sort removes one element from the input data, fi
+//nds the location it belongs within the sorted list, and inserts it there. 
+// It repeats until no input elements remain. 
+// 
+//
+// 
+//Example 1: 
+//
+// 
+//Input: 4->2->1->3
+//Output: 1->2->3->4
+// 
+//
+// Example 2: 
+//
+// 
+//Input: -1->5->3->4->0
+//Output: -1->0->3->4->5
+// 
+// Related Topics Linked List Sort
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode dummy = new ListNode(0), cur = head, pre = dummy, next;
+
+        while (cur != null) {
+            next = cur.next;
+
+            while (pre.next != null && pre.next.val < cur.val) {
+                pre = pre.next;
+            }
+
+            cur.next = pre.next;
+            pre.next = cur;
+            pre = dummy;
+            cur = next;
+        }
+
+        return dummy.next;
+    }
+
+//    public class ListNode {
+//        int val;
+//        ListNode next;
+//        ListNode(int x) { val = x; }
+//    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
